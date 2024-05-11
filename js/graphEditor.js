e1ea7e7b4ea09c3ada1009b6287cef4896955521
badwaikparas <111993734+badwaikparas@users.noreply.github.com>
2024-05-11 21:29:40 +0530

    line to cursor from selected point

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -8,6 +8,7 @@ class GraphEditor {
     this.selected = null;
     this.hovered = null;
     this.dragging = false;
+    this.mouse = null;
 
     this.#addEventListeners();
   }
@@ -24,24 +25,24 @@ class GraphEditor {
       }
       if (evt.button == 0) {
         // left click
-        const mouse = new Point(evt.offsetX, evt.offsetY);
+        // const mouse = new Point(evt.offsetX, evt.offsetY);
         // this.hovered = getNearestPoint(mouse, this.graph.points, 38);
         if (this.hovered) {
           this.#select(this.hovered);
           this.dragging = true;
           return;
         }
-        this.graph.addPoint(mouse);
-        this.#select(mouse);
-        this.hovered = mouse;
+        this.graph.addPoint(this.mouse);
+        this.#select(this.mouse);
+        this.hovered = this.mouse;
       }
     });
     this.canvas.addEventListener("mousemove", (evt) => {
-      const mouse = new Point(evt.offsetX, evt.offsetY);
-      this.hovered = getNearestPoint(mouse, this.graph.points, 38);
+      this.mouse = new Point(evt.offsetX, evt.offsetY);
+      this.hovered = getNearestPoint(this.mouse, this.graph.points, 38);
       if (this.dragging) {
-        this.selected.x = mouse.x;
-        this.selected.y = mouse.y;
+        this.selected.x = this.mouse.x;
+        this.selected.y = this.mouse.y;
       }
     });
     this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
@@ -69,6 +70,7 @@ class GraphEditor {
       this.hovered.draw(this.ctx, { fill: true });
     }
     if (this.selected) {
+      new Segment(this.selected, this.mouse).draw(ctx);
       this.selected.draw(this.ctx, { outline: true });
     }
   }
