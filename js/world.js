d33bc1431e83bf3b1fad465ff59fba2e0a70e359
badwaikparas <badwaikparas@gmail.com>
2025-03-29 06:06:17 +0530

    trees3D done

--- js/world.js
+++ js/world.js
@@ -73,7 +73,7 @@ class World {
             // are trees too close to other trees
             if (keep) {
                 for (const tree of trees) {
-                    if (distance(tree, p) < this.treeSize) {
+                    if (distance(tree.center, p) < this.treeSize) {
                         keep = false
                         break
                     }
@@ -93,7 +93,7 @@ class World {
             }
 
             if (keep) {
-                trees.push(p)
+                trees.push(new Tree(p, this.treeSize))
                 tryCount = 0
             }
 
@@ -164,7 +164,7 @@ class World {
         return bases
     }
 
-    draw(ctx) {
+    draw(ctx, viewPoint) {
         for (const env of this.envelopes) {
             env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 })
         }
@@ -178,7 +178,7 @@ class World {
             bld.draw(ctx)
         }
         for (const tree of this.trees) {
-            tree.draw(ctx, { color: "rgba(0,0,0,0.5)", size: this.treeSize })
+            tree.draw(ctx, viewPoint)
         }
     }
 
